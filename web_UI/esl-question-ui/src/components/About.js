16fe7c0fb9909fb3e00591290e38bcf426b8b5f0
Abigail Byram <byramabigail@gmail.com>
2020-11-30 19:04:12 -0500

    Cleanup simple pages

--- web_UI/esl-question-ui/src/components/About.js
+++ web_UI/esl-question-ui/src/components/About.js
@@ -2,20 +2,21 @@
  About page of application: talks about the research, implications, assumptions, and future work
 */
 import { Link } from 'react-router';
+import React from 'react';
 import '../css/App.css';
 
-function About() {
+export default class About extends React.Component {
 
-  return (
-    <div className="About">
-      <body className="App-body">
-        <h1>About Page</h1>
-        <p>
-          This is about page
-        </p>
-      </body>
-    </div>
-  );
+  render() {
+    return (
+      <div className="About">
+        <body className="App-body">
+          <h1>About Page</h1>
+          <p>
+            This is about page
+          </p>
+        </body>
+      </div>
+    );
+  }
 }
-
-export default About;
